dcde7222763019bd224298b435dfb20738ed6052
lise6262 <75783788+lise6262@users.noreply.github.com>
2023-06-06 10:45:13 +0200

    back

---
+++ src/server/party-event/party-event/src/main/java/com/partyevent/PartyEventsApplication.java
@@ -0,0 +1,13 @@
+package com.partyevent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PartyEventsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PartyEventsApplication.class, args);
+	}
+
+}
